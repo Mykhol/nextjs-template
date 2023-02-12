6f8033723227f121588c590869d948222a871edd
Michael Howell <mh.michaelhowell@gmail.com>
2023-02-12 20:12:42 +1300

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,17 @@
-import './globals.css'
+import '@/styles/globals.css'
+import React from "react";
 
-export default function RootLayout({
-  children,
-}: {
+const RootLayout = ({
+                      children,
+                    }: {
   children: React.ReactNode
-}) {
+}) => {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+    <head/>
+    <body>{children}</body>
     </html>
   )
 }
+
+export default RootLayout;
